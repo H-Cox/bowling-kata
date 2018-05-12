7f11646976eaed5c8c8daf0fbe03b084fac79e72
Henry Cox <henry.cox@manchester.ac.uk>
2018-05-12 20:12:48 +0100

    modified tests 3 and 4 to extract score properly

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -24,22 +24,23 @@ describe BowlingGame do
     context "given zero" do
       it "returns zero" do
       	game = BowlingGame.new
-      	score = game.bowl(0)
-        expect(score).to eql(0)
+      	game.bowl(0)
+        expect(game.score).to eql(0)
       end
     end
 
     context "given 1" do
       it "returns 1" do
       	game = BowlingGame.new
-      	score = game.bowl(1)
-        expect(score).to eql(1)
+      	game.bowl(1)
+        expect(game.score).to eql(1)
       end
     end 
 
     context "score of 10 is a strike and go to next round" do
       it "round increases" do
       	game = BowlingGame.new
+      	game.startGame
       	game.bowl(10)
         expect(game.round).to eql(2)
       end
@@ -48,6 +49,7 @@ describe BowlingGame do
     context "score of less than 10 doesn't increase round" do
       it "round increases" do
       	game = BowlingGame.new
+      	game.startGame
       	game.bowl(9)
         expect(game.round).to eql(1)
       end
