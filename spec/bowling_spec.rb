4dfc151204ec25b4c7616d22eda626519725ecf7
Henry Cox <henry.cox@manchester.ac.uk>
2018-05-12 19:52:04 +0100

    1st test

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,5 +1,14 @@
 require_relative '../src/bowling'
 
 describe BowlingGame do
-	
+	describe ".bowl" do
+    context "given zero" do
+      it "returns zero" do
+      	game = BowlingGame.new
+      	score = game.bowl(0)
+
+        expect(score).to eql(0)
+      end
+    end
+  end
 end
--- src/bowling.rb
+++ src/bowling.rb
@@ -1,2 +1,3 @@
 class BowlingGame
+
 end
